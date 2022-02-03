90469f409b1b16609a3c5bbc8e46d80f93f0279e
adilallo <39313955+adilallo@users.noreply.github.com>
2022-02-03 11:12:31 -0700

    libpd

---
+++ Assets/FinalGame/Scripts/GlobalVariables.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalVariables : MonoBehaviour
+{
+    // Input Constants
+    public KeyCode upCrank = KeyCode.UpArrow;
+    public KeyCode downCrank = KeyCode.DownArrow;
+    public KeyCode leftSlider = KeyCode.LeftArrow;
+    public KeyCode rightSlider = KeyCode.RightArrow;
+    public KeyCode knobLeft = KeyCode.Comma;
+    public KeyCode knobRight = KeyCode.Period;
+    public KeyCode deviceButton = KeyCode.Space;
+
+    // We also need to keep track of constants from the Device & Window
+    // Speech Tuning Constants
+    
+    
+    // Plant Care Constants?
+    
+    // Singleton
+    public static GlobalVariables S;
+    
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (S == null) {
+            S = this;
+        } else {
+            DestroyObject(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
