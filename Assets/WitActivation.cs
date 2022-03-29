2ce350f445b7989a6e4948f1ce45b40663f443d1
m <matt@mouseandthebillionaire.com>
2022-03-29 07:49:58 -0600

    Switch STT implementation to Wit.io

---
+++ Assets/WitActivation.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Facebook.WitAi;
+
+public class WitActivation : MonoBehaviour
+{
+    [SerializeField] private Wit wit;
+
+    private void OnValidate()
+    {
+        if(!wit) wit = GetComponent<Wit>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Debug.Log("Listening...");
+            wit.Activate();
+        }
+    }
+}
